5ead9e9d575fb0464e0ad5e4601fef1bbeaf1076
nhternup <nhternup@users.noreply.github.com>
2019-09-27 09:23:53 +0530

    Add toggle for local and server image view

--- app/src/main/java/io/ffem/lite/ui/ResultActivity.kt
+++ app/src/main/java/io/ffem/lite/ui/ResultActivity.kt
@@ -13,26 +13,26 @@ import java.io.File
 
 class ResultActivity : BaseActivity() {
 
+    private lateinit var localPath: String
+    private lateinit var serverPath: String
+    private var localMode = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
         setTitle(R.string.app_name)
 
-        val id = intent.getStringExtra(TEST_ID_KEY)
-        val name = intent.getStringExtra(TEST_NAME_KEY)
+        val id = intent.getStringExtra(TEST_ID_KEY)!!
+        val name = intent.getStringExtra(TEST_NAME_KEY)!!
 
         val path = getExternalFilesDir(DIRECTORY_PICTURES).toString() +
                 File.separator + "captures" + File.separator
 
-        val file = File(path + id + "_" + ".jpg")
-        if (file.exists()) {
-            image.setImageURI(Uri.fromFile(file))
-        }
-    }
+        localPath = path + id + "_" + ".jpg"
+        serverPath = path + id + "_" + name + ".jpg"
 
-    fun onHomeClick(@Suppress("UNUSED_PARAMETER") view: View) {
-        finish()
+        showLocalImage()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -41,5 +41,34 @@ class ResultActivity : BaseActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun showLocalImage() {
+        val file = File(localPath)
+        if (file.exists()) {
+            image.setImageURI(Uri.fromFile(file))
+        }
+        imageModeButton.text = "Show server image"
+        localMode = true
+    }
+
+    private fun showServerImage() {
+        val file = File(serverPath)
+        if (file.exists()) {
+            image.setImageURI(Uri.fromFile(file))
+        }
+        imageModeButton.text = "Show local image"
+        localMode = false
+    }
+
+    fun onHomeClick(@Suppress("UNUSED_PARAMETER") view: View) {
+        finish()
+    }
+
+    fun onImageTypeClick(@Suppress("UNUSED_PARAMETER") view: View) {
+        when {
+            localMode -> showServerImage()
+            else -> showLocalImage()
+        }
     }
 }
