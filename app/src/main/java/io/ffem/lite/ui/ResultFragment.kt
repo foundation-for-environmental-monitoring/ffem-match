d259bdd895eccaadbfcce1e59c7cdd61ef4ec05d
nhternup <nhternup@users.noreply.github.com>
2020-03-31 10:14:40 +0530

    [#23] Display risk level on the result screen

--- app/src/main/java/io/ffem/lite/ui/ResultFragment.kt
+++ app/src/main/java/io/ffem/lite/ui/ResultFragment.kt
@@ -24,6 +24,7 @@ class ResultFragment : Fragment() {
         text_result.text = testInfo.getResultString(view.context)
         if (testInfo.error == ErrorType.NO_ERROR) {
             text_unit.text = testInfo.unit
+            text_risk.text = getString(R.string.risk_value, testInfo.getRisk(context!!))
         }
         super.onViewCreated(view, savedInstanceState)
     }
