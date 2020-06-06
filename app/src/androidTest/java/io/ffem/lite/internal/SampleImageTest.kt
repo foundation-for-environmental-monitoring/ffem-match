fdd3932cbbdb8dcf30693a1a8f4245c1c21a21f1
SrijanaB <58282829+srijana-ya@users.noreply.github.com>
2020-06-07 01:49:45 +0530

    [#39] Instrumented test for fluoride added

--- app/src/androidTest/java/io/ffem/lite/internal/SampleImageTest.kt
+++ app/src/androidTest/java/io/ffem/lite/internal/SampleImageTest.kt
@@ -187,6 +187,11 @@ class SampleImageTest {
     }
 
     @Test
+    fun image_024_Fluoride_0_Point_5() {
+        startInternalTest(24)
+    }
+
+    @Test
     fun imageX_Waiting() {
         startInternalTest(500)
     }
