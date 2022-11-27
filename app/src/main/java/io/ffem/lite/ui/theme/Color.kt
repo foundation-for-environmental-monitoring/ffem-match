cfad58aa90a307772ac134ee50a07e6e76c3d72e
nhternup <nhternup@users.noreply.github.com>
2022-11-27 20:11:18 +0530

    UI colors update, Increment version

--- app/src/main/java/io/ffem/lite/ui/theme/Color.kt
+++ app/src/main/java/io/ffem/lite/ui/theme/Color.kt
@@ -5,5 +5,5 @@ import androidx.compose.ui.graphics.Color
 val Gray80 = Color(0xFF000000)
 val GrayGrey80 = Color(0xFF000000)
 
-val Gray40 = Color(0xFF838383)
-val GrayGrey40 = Color(0xFF838383)
+val Gray40 = Color(0xFF00698C)
+val GrayGrey40 = Color(0xFF00698C)
